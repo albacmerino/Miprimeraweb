755477f3452b2df0493ea5051c9f4b9833868833
albacmerino <muprespas@hotmail.com>
2024-10-20 01:13:56 +0200

    Cambie el index

--- scripts/navegaIndex.sh
+++ scripts/navegaIndex.sh
@@ -2,7 +2,15 @@
 
 cat << EOF
 
-echo "<nav>
+echo "<!DOCTYPE html>
+<html lang='es'>
+    <head>
+        <meta charset='UTF-8'>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Mi página web.</title>
+    </head>
+    <body>
+        <nav>
             <ul>
                 <li><a href='index.html'>Inicio</a></li>
                 <li><a href='pagina1.html'>Página 1</a></li>
@@ -10,5 +18,9 @@ echo "<nav>
                 <li><a href='pagina3.html'>Página 3</a></li>
             </ul>
         </nav>
-    </body>"
+    </body>
+    <footer>
+        <p>Web Alba Carrobles Merino</p>
+      </footer>
+</html>"
 EOF
