6621a17276f2148e5c2097d35cc0894a1d270898
albacmerino <muprespas@hotmail.com>
2024-10-20 03:04:56 +0200

    Agrege un cambio

--- navegaPag1.sh
+++ navegaPag1.sh
@@ -4,9 +4,9 @@ cat << EOF
         <nav>
             <p>Este es el contenido de la primera página.</p>
             <ul>
-                <li><a href='index.html'>Índice</a></li>
-                <li><a href='pagina2.html'>Página 2</a></li>
-                <li><a href='pagina3.html'>Página 3</a></li>
+                <li><a href='./index.html'>Índice</a></li>
+                <li><a href='./web/pagina2.html'>Página 2</a></li>
+                <li><a href='./web/pagina3.html'>Página 3</a></li>
             </ul>
         </nav>
     </body>
