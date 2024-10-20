c208f361b6fbf09ed5ad0e866a3c04153976bc39
albacmerino <muprespas@hotmail.com>
2024-10-20 03:23:43 +0200

    He cambiado los navegadores

--- navegaPag3.sh
+++ navegaPag3.sh
@@ -4,7 +4,7 @@ cat << EOF
         <nav>
             <p>Este es el contenido de la tercera página.</p>
             <ul>
-                <li><a href='index.html'>Índice</a></li>
+                <li><a href='./index.html'>Índice</a></li>
                 <li><a href='pagina1.html'>Página 1</a></li>
                 <li><a href='pagina2.html'>Página 2</a></li>
             </ul>
