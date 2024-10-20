c638ce2b4d198b64737ceff878b614bb7898081d
albacmerino <muprespas@hotmail.com>
2024-10-20 03:27:28 +0200

    He cambiado los navegadores

--- navegaPag1.sh
+++ navegaPag1.sh
@@ -4,7 +4,7 @@ cat << EOF
         <nav>
             <p>Este es el contenido de la primera página.</p>
             <ul>
-                <li><a href='./index.html'>Índice</a></li>
+                <li><a href='../index.html'>Índice</a></li>
                 <li><a href='pagina2.html'>Página 2</a></li>
                 <li><a href='pagina3.html'>Página 3</a></li>
             </ul>
